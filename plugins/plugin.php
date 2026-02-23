e7b000da9e99ceaffe6ed37442ed5ac13761f0c5
rashedsmiya <rashedmiyaofficial@gmail.com>
2026-02-23 18:27:34 +0600

    Update plugin.php

--- plugins/plugin.php
+++ plugins/plugin.php
@@ -5,7 +5,9 @@
    };
 
    $images_section = function(){
+      
     echo "<div>
     <img src='https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg' alt='' style='max-width: 100%;'>
     </div>";
+
    };
